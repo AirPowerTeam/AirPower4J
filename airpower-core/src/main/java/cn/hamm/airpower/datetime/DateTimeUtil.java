7549dd953f480cde95e838467cf84b9ed0bb51b1
Hamm <majhamm@qq.com>
2025-11-06 23:54:48 +0800

    feat(datetime): 增强日期时间工具类功能
    - 添加时间戳和字符串转换为日期的方法
    - 实现获取当前年、月、日、时、分、秒的静态方法
    - 添加日期加减操作方法，支持天、小时、分钟、秒、毫秒、周、月、年
    - 实现获取月份和年份起始日期的方法
    - 引入 Calendar 和 Date 相关类以支持日期计算功能
    
    Signed-off-by: Hamm <majhamm@qq.com>

--- airpower-core/src/main/java/cn/hamm/airpower/datetime/DateTimeUtil.java
+++ airpower-core/src/main/java/cn/hamm/airpower/datetime/DateTimeUtil.java
@@ -4,9 +4,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
+import java.text.ParseException;
 import java.time.Instant;
+import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.util.Calendar;
+import java.util.Date;
 
 import static cn.hamm.airpower.datetime.DateTimeFormatter.FULL_DATETIME;
 
@@ -142,6 +146,30 @@ public class DateTimeUtil {
     }
 
     /**
+     * 时间戳转换成日期格式
+     *
+     * @param milliSecond 毫秒时间戳
+     * @return 时间戳对应的日期
+     */
+    @Contract(value = "_ -> new", pure = true)
+    public static @NotNull Date parse(long milliSecond) {
+        return new Date(milliSecond);
+    }
+
+    /**
+     * 字符串转换成日期格式
+     *
+     * @param dateTime 字符串
+     * @return 时间戳对应的日期
+     */
+    @Contract(value = "_ -> new", pure = true)
+    public static @NotNull Date parse(String dateTime) throws ParseException {
+        java.time.format.DateTimeFormatter formatter = java.time.format.DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDateTime localDateTime = LocalDateTime.parse(dateTime, formatter);
+        return Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
+    }
+
+    /**
      * 友好格式化时间
      *
      * @param milliSecond 毫秒时间戳
@@ -180,5 +208,294 @@ public class DateTimeUtil {
             }
         }
         return "未知时间";
+    }
+
+    /**
+     * 获取当前年份
+     *
+     * @return 年份
+     */
+    public static int getCurrentYear() {
+        return ZonedDateTime.now().getYear();
+    }
+
+    /**
+     * 获取当前月份
+     *
+     * @return 月份
+     */
+    public static int getCurrentMonth() {
+        return ZonedDateTime.now().getMonthValue();
+    }
+
+    /**
+     * 获取当前日期
+     *
+     * @return 日期
+     */
+    public static int getCurrentDay() {
+        return ZonedDateTime.now().getDayOfMonth();
+    }
+
+    /**
+     * 获取当前小时
+     *
+     * @return 小时
+     */
+    public static int getCurrentHour() {
+        return ZonedDateTime.now().getHour();
+    }
+
+    /**
+     * 获取当前分钟
+     *
+     * @return 分钟
+     */
+    public static int getCurrentMinute() {
+        return ZonedDateTime.now().getMinute();
+    }
+
+    /**
+     * 获取当前秒
+     *
+     * @return 秒
+     */
+    public static int getCurrentSecond() {
+        return ZonedDateTime.now().getSecond();
+    }
+
+    /**
+     * 添加时间
+     *
+     * @param date          日期
+     * @param calendarField 日历属性
+     * @param amount        数量
+     * @return 新日期
+     */
+    private static @NotNull Date add(Date date, int calendarField, int amount) {
+        Calendar c = Calendar.getInstance();
+        c.setTime(date);
+        //noinspection MagicConstant
+        c.add(calendarField, amount);
+        return c.getTime();
+    }
+
+    /**
+     * 添加天
+     *
+     * @param date   日期
+     * @param amount 天数
+     * @return 新日期
+     */
+    public static @NotNull Date addDays(Date date, int amount) {
+        return add(date, Calendar.DATE, amount);
+    }
+
+    /**
+     * 添加天
+     *
+     * @param amount 天数
+     * @return 新日期
+     */
+    public static @NotNull Date addDays(int amount) {
+        return addDays(new Date(), amount);
+    }
+
+    /**
+     * 添加小时
+     *
+     * @param date   日期
+     * @param amount 小时数
+     * @return 新日期
+     */
+    public static @NotNull Date addHours(Date date, int amount) {
+        return add(date, Calendar.HOUR_OF_DAY, amount);
+    }
+
+    /**
+     * 添加小时
+     *
+     * @param amount 小时数
+     * @return 新日期
+     */
+    public static @NotNull Date addHours(int amount) {
+        return addHours(new Date(), amount);
+    }
+
+    /**
+     * 添加毫秒
+     *
+     * @param date   日期
+     * @param amount 毫秒数
+     * @return 新日期
+     */
+    public static @NotNull Date addMilliseconds(Date date, int amount) {
+        return add(date, Calendar.MILLISECOND, amount);
+    }
+
+    /**
+     * 添加毫秒
+     *
+     * @param amount 毫秒数
+     * @return 新日期
+     */
+    public static @NotNull Date addMilliseconds(int amount) {
+        return addMilliseconds(new Date(), amount);
+    }
+
+    /**
+     * 添加分钟
+     *
+     * @param date   日期
+     * @param amount 分钟数
+     * @return 新日期
+     */
+    public static @NotNull Date addMinutes(Date date, int amount) {
+        return add(date, Calendar.MINUTE, amount);
+    }
+
+    /**
+     * 添加分钟
+     *
+     * @param amount 分钟数
+     * @return 新日期
+     */
+    public static @NotNull Date addMinutes(int amount) {
+        return addMinutes(new Date(), amount);
+    }
+
+    /**
+     * 添加月
+     *
+     * @param date   日期
+     * @param amount 月数
+     * @return 新日期
+     */
+    public static @NotNull Date addMonths(Date date, int amount) {
+        return add(date, Calendar.MONTH, amount);
+    }
+
+    /**
+     * 添加月
+     *
+     * @param amount 月数
+     * @return 新日期
+     */
+    public static @NotNull Date addMonths(int amount) {
+        return addMonths(new Date(), amount);
+    }
+
+    /**
+     * 添加秒
+     *
+     * @param date   日期
+     * @param amount 秒数
+     * @return 新日期
+     */
+    public static @NotNull Date addSeconds(Date date, int amount) {
+        return add(date, Calendar.SECOND, amount);
+    }
+
+    /**
+     * 添加秒
+     *
+     * @param amount 秒数
+     * @return 新日期
+     */
+    public static @NotNull Date addSeconds(int amount) {
+        return addSeconds(new Date(), amount);
+    }
+
+    /**
+     * 添加周
+     *
+     * @param date   日期
+     * @param amount 周数
+     * @return 新日期
+     */
+    public static @NotNull Date addWeeks(Date date, int amount) {
+        return add(date, Calendar.WEEK_OF_YEAR, amount);
+    }
+
+    /**
+     * 添加周
+     *
+     * @param amount 周数
+     * @return 新日期
+     */
+    public static @NotNull Date addWeeks(int amount) {
+        return addWeeks(new Date(), amount);
+    }
+
+    /**
+     * 添加年
+     *
+     * @param date   日期
+     * @param amount 年数
+     * @return 新日期
+     */
+    public static @NotNull Date addYears(Date date, int amount) {
+        return add(date, Calendar.YEAR, amount);
+    }
+
+    /**
+     * 添加年
+     *
+     * @param amount 年数
+     * @return 新日期
+     */
+    public static @NotNull Date addYears(int amount) {
+        return addYears(new Date(), amount);
+    }
+
+    /**
+     * 获取指定日期所在月份的第一天
+     *
+     * @param date 日期
+     * @return 新日期
+     */
+    public static @NotNull Date getStartOfMonth(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.DAY_OF_MONTH, 1);
+        return calendar.getTime();
+    }
+
+    /**
+     * 获取当前月份开始的日期时间
+     *
+     * @return 当月开始
+     */
+    public static @NotNull Date getStartOfMonth() {
+        return getStartOfMonth(new Date());
+    }
+
+    /**
+     * 获取指定日期所在年的第一天
+     *
+     * @param date 日期
+     * @return 新日期
+     */
+    public static @NotNull Date getStartOfYear(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        calendar.set(Calendar.DAY_OF_YEAR, 1);
+        return calendar.getTime();
+    }
+
+    /**
+     * 获取当前年的第一天
+     *
+     * @return 当年开始
+     */
+    public static @NotNull Date getStartOfYear() {
+        return getStartOfYear(new Date());
     }
 }

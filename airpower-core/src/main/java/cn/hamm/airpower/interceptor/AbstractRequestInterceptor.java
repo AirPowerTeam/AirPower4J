ad2182c282840a41b597224fb389d0cc74ae836d
Hamm <majhamm@qq.com>
2025-12-27 15:59:59 +0800

    refactor(interceptor): 移除未使用的API配置依赖
    - 移除了AbstractRequestInterceptor中的ApiConfig导入
    - 移除了API_DOWN异常的静态导入
    - 移除了服务器运行状态检查代码
    - 简化了请求拦截器的初始化逻辑
    
    Signed-off-by: Hamm <majhamm@qq.com>

--- airpower-core/src/main/java/cn/hamm/airpower/interceptor/AbstractRequestInterceptor.java
+++ airpower-core/src/main/java/cn/hamm/airpower/interceptor/AbstractRequestInterceptor.java
@@ -4,7 +4,6 @@ import cn.hamm.airpower.access.Access;
 import cn.hamm.airpower.access.AccessConfig;
 import cn.hamm.airpower.access.AccessTokenUtil;
 import cn.hamm.airpower.access.PermissionUtil;
-import cn.hamm.airpower.api.ApiConfig;
 import cn.hamm.airpower.request.HttpConstant;
 import cn.hamm.airpower.util.TraceUtil;
 import jakarta.servlet.http.HttpServletRequest;
@@ -22,7 +21,6 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import java.lang.reflect.Method;
 import java.util.Objects;
 
-import static cn.hamm.airpower.exception.ServiceError.API_DOWN;
 import static cn.hamm.airpower.exception.ServiceError.UNAUTHORIZED;
 
 /**
@@ -65,7 +63,6 @@ public abstract class AbstractRequestInterceptor implements HandlerInterceptor {
         String traceId = request.getHeader(HttpConstant.Header.TRACE_ID);
         TraceUtil.setTraceId(traceId);
         log.info("请求地址 {}", request.getRequestURI());
-        API_DOWN.when(!ApiConfig.isServerRunning);
         HandlerMethod handlerMethod = (HandlerMethod) object;
         //取出控制器和方法
         Class<?> clazz = handlerMethod.getBeanType();

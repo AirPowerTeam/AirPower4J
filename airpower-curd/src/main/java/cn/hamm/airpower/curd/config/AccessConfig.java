3de6d168c884115ad2726e2901e2fbd9bf3e5970
hamm <hamm@example.com>
2026-02-16 22:44:32 +0800

    fix(bugs): 修复了一些已知的BUG

--- airpower-curd/src/main/java/cn/hamm/airpower/curd/config/AccessConfig.java
+++ airpower-curd/src/main/java/cn/hamm/airpower/curd/config/AccessConfig.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Configuration;
  */
 @Data
 @Configuration
-@ConfigurationProperties("airpower.access")
+@ConfigurationProperties("airpower.curd.access")
 public class AccessConfig {
 
     /**
